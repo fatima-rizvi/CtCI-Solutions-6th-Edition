a5d0813abd7b468e88f06707ccb7992bc8fe5c70
Fatima Rizvi <68958153+fatima-rizvi@users.noreply.github.com>
2021-03-11 20:46:13 -0600

    Update 01_remove_dups.py

--- Ch2_Linked_Lists/01_remove_dups.py
+++ Ch2_Linked_Lists/01_remove_dups.py
@@ -1,53 +1,6 @@
 # Write code to remove duplicates from an unsorted linked list
 
-class Node: 
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-    def get_value(self):
-        return self.value
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def add_to_tail(self, value):
-        new_node = Node(value)
-        if self.head is None: 
-            self.head = new_node
-            self.tail = new_node
-            return
-        self.tail.next = new_node
-        self.tail = new_node
-
-    def add_to_head(self, value):
-        new_node = Node(value) 
-        if self.head is None:  
-            self.head = new_node
-            self.tail = new_node
-            return
-        old_head = self.head    
-        self.head = new_node   
-        self.head.next = old_head 
-
-    def remove_head(self):
-        if self.head is None:
-            return
-        data = self.head.get_value()
-        self.head = self.head.next
-        return data
-
-    def remove_tail(self):
-        data = self.tail.get_value()
-        cursor = self.head  
-        while cursor.next.next is not None:  
-            cursor = cursor.next    
-        self.tail = cursor   
-        self.tail.next = None   
-        return data
-
+from utils import LinkedList
 
 # List visual: [5, 2, 3, 7, 7, 8, 1, 2, 9]
 linked_list = LinkedList()
