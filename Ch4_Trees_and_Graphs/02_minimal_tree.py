7bd66c6838be85f678d1a5df5630c1227989ba1c
Fatima Rizvi <68958153+fatima-rizvi@users.noreply.github.com>
2021-03-19 19:54:40 -0500

    Update 02_minimal_tree.py

--- Ch4_Trees_and_Graphs/02_minimal_tree.py
+++ Ch4_Trees_and_Graphs/02_minimal_tree.py
@@ -1,1 +1,21 @@
 # Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
+
+from utils import Node
+
+def minimal_tree(arr):
+    if len(arr) == 0:
+        return None
+    
+    mid = len(arr) // 2
+    
+    current = Node(arr[mid])
+    current.left = minimal_tree(arr[:mid])
+    current.right = minimal_tree(arr[mid + 1:])
+    
+
+    return current.value # Can just print the current node as a whole
+
+
+tester = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+print(minimal_tree(tester))
