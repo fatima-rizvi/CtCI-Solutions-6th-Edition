8ea27251b81b86e2e8efe0e5c9c8ab6a53d5fd0d
Fatima Rizvi <68958153+fatima-rizvi@users.noreply.github.com>
2021-03-22 13:11:41 -0500

    Update utils.py

--- Ch4_Trees_and_Graphs/utils.py
+++ Ch4_Trees_and_Graphs/utils.py
@@ -3,3 +3,29 @@ class Node:
         self.value = val
         self.left = None
         self.right = None
+        
+class ListNode():
+  def __init__(self, data=None, next=None):
+    self.data, self.next = data, next
+  
+  def __str__(self):
+    return str(self.data) + ',' + str(self.next)
+
+class Queue():
+  def __init__(self):
+    self.head, self.tail = None, None
+  
+  def add(self, item):
+    if self.head:
+      self.tail.next = ListNode(item)
+      self.tail = self.tail.next
+    else:
+      self.head = self.tail = ListNode(item)
+  
+  def remove(self):
+    if not self.head:
+      return None
+    item = self.head.data
+    self.head = self.head.next
+    return item
+
